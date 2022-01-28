d39b64ba3ea2c0c359713a0d876a25af9151c95f
MamatovRashid <mamatovrashids@gmail.com>
2022-01-28 12:11:15 +0500

    2-dars vueclida yaratildi tailwindcss qo'shildi

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <lesson />
+  </div>
+</template>
+
+<script>
+import Lesson from "./views/2-lesson.vue";
+export default {
+  components: { Lesson },
+  name: "App",
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/2-lesson.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <div class="max-w-xl mx-auto pt-20">
+      <h1 class="text-8xl text-center text-blue-600 mb-4">todos</h1>
+      <div class="bg-white ">
+        <div class="flex items-center">
+          <i></i>
+          <input type="text" class="text-2xl p-3 outline-none flex-1" placeholder="What needs to be done" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+};
+</script>
+
+<style lang="scss" scoped></style>
