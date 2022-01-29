671ed7f69b094083d3b2f38a76c02a51f12727d1
MamatovRashid <mamatovrashids@gmail.com>
2022-01-29 17:15:16 +0500

    funksiyalarni qaytanomlashim kerak

--- src/views/2-lesson.vue
+++ src/views/2-lesson.vue
@@ -4,12 +4,28 @@
       <h1 class="text-8xl text-center text-blue-600 mb-4">todos</h1>
       <div class="bg-white shadow-xl">
         <div class="flex items-center">
-          <button class="transform text-2xl -rotate-90 px-3 text-gray-300">&#10094;</button>
+          <button @click="allTodosDone" class="transform text-2xl -rotate-90 px-3 text-gray-300" :class="{ 'text-gray-900': allTodosCompleted }">&#10094;</button>
           <input v-model="inputTodo" @keydown.enter="addTodos" type="text" class="text-2xl p-3 outline-none flex-1" placeholder="What needs to be done" />
         </div>
         <ul class="border-t">
-          <todo v-for="todo in todos" :key="todo.id" :todo="todo" @done-change="changeDone(todo.id)" />
+          <todo v-for="(todo, index) in filteredTodos" :key="todo.id" :todo="todo" @done-change="changeDone(todo.id)" @delete="deleteTodo(index)" @edit="editText(todo.id, text)" />
         </ul>
+        <div class="flex items-center p-2 text-gray-500">
+          <div class="flex-1">
+            <p>{{ activeCountText }}</p>
+          </div>
+          <div>
+            <input type="radio" class="hidden" v-model="filter" id="ALL" value="ALL" />
+            <label class="py-0.5 px-1.5 mx-1 border rounded border-transparent cursor-pointer" :class="{ 'border-gray-200': filter === 'ALL' }" for="ALL">All</label>
+            <input type="radio" class="hidden" v-model="filter" id="ACTIVE" value="ACTIVE" />
+            <label class="py-0.5 px-1.5 mx-1 border rounded border-transparent cursor-pointer" :class="{ 'border-gray-200': filter === 'ACTIVE' }" for="ACTIVE">Active</label>
+            <input type="radio" class="hidden" v-model="filter" id="COMPLETED" value="COMPLETED" />
+            <label class="py-0.5 px-1.5 mx-1 border rounded border-transparent cursor-pointer" :class="{ 'border-gray-200': filter === 'COMPLETED' }" for="COMPLETED">Completed</label>
+          </div>
+          <div class="flex-1 flex justify-end">
+            <button @click="clearDoneTodos" v-show="doneTodos">Clear completed</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -23,17 +39,52 @@ export default {
   data: () => ({
     todos: [],
     inputTodo: "",
+    left: "",
+    filter: "ALL",
   }),
+  computed: {
+    doneTodos() {
+      if (this.todos.filter((todo) => todo.done).length) {
+        return true;
+      }
+      return false;
+    },
+    allTodosCompleted() {
+      if (this.todos.filter((todo) => !todo.done).length || !this.todos.length) {
+        return false;
+      }
+      return true;
+    },
+    activeCountText() {
+      const count = this.todos.filter((todo) => !todo.done).length;
+      if (count === 1) {
+        return "1 item left";
+      }
+      return `${count} items left`;
+    },
+    filteredTodos() {
+      if (this.filter === "ALL") {
+        return this.todos;
+      }
+      return this.todos.filter((todo) => (this.filter === "ACTIVE" ? todo.done : !todo.done));
+    },
+  },
   methods: {
     addTodos() {
       if (this.inputTodo) {
         this.todos.push({
-          id: this.todos.length + 1,
+          id: this.todos.length + 2,
           text: this.inputTodo,
           done: false,
         });
         this.inputTodo = "";
       }
+    },
+    allTodosDone() {
+      const doneTodo = this.todos.filter((todo) => todo.done).length === this.todos.length;
+      this.todos = this.todos.map((todo) => {
+        return { ...todo, done: doneTodo ? false : true };
+      });
     },
     changeDone(todoId) {
       this.todos = this.todos.map((todo) => {
@@ -43,6 +94,25 @@ export default {
           return todo;
         }
       });
+    },
+    clearDoneTodos() {
+      this.todos = this.todos.filter(todo => !todo.done);
+    },
+    editText(todoId, e) {
+      console.log(todoId, e);
+      // this.todos = this.todos.map((todo) => {
+      //   if (todo.id === todoId) {
+      //     return { ...todo, text: event };
+      //   } else {
+      //     return todo;
+      //   }
+      // });
+    },
+    // filterActive(){
+    //   this.todos = this.todos.filter(todo =
+    // },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
     },
   },
 };
--- src/views/components/todo.vue
+++ src/views/components/todo.vue
@@ -1,12 +1,13 @@
 <template>
   <li class="text-xl flex items-center border-b todo">
-    <input type="checkbox" class="hidden" :id="todo.id" :checked="todo.done" @change="onCheckboxChange"/>
+    <input type="checkbox" class="hidden" :id="todo.id" :checked="todo.done" @change="onCheckboxChange" />
     <label :for="todo.id" class="w-9 h-9 border cursor-pointer flex items-center justify-center rounded-full mx-2"><span v-show="todo.done" class="text-green-500">&#10003;</span></label>
-    <div class="py-4 flex-1 border-r border-l border-transparent" :class="{ 'border-gray-200': edit }">
-      <input class="outline-none w-full" v-show="edit" @blur="edit = false" type="text" v-model="todo.text" />
-      <p v-show="!edit" @dblclick="edit = true" class="w-full h-full " :class="{'text-gray-300 line-through': todo.done}">{{ todo.text }}</p>
+    <div class="py-4 flex-1 px-2 border-r border-l border-transparent" :class="{ 'border-gray-200': edit }">
+      <input ref="editInput" class="outline-none w-full " v-if="edit" @blur="edit = false" type="text" v-model="todo.text" />
+      
+      <p v-else @dblclick="openEditInput" class="w-full h-full" :class="{ 'text-gray-300 line-through': todo.done }">{{ todo.text }}</p>
     </div>
-    <button class="mr-4 cancel" v-show="!edit">&#10005;</button>
+    <button @click="deleteTodo" class="mr-4 cancel" v-show="!edit">&#10005;</button>
   </li>
 </template>
 
@@ -15,13 +16,29 @@ export default {
   name: "todo",
   data: () => ({
     edit: false,
+    text: ''
   }),
   props: ["todo"],
+  mounted(){
+    this.text = this.todo.text
+  },
   methods: {
-      onCheckboxChange(){
-          this.$emit('done-change')
-      }
-  }
+    onCheckboxChange() {
+      this.$emit("done-change");
+    },
+    deleteTodo() {
+      this.$emit("delete");
+    },
+    editText(text) {
+      this.$emit("edit", text);
+    },
+    openEditInput() {
+      !this.todo.done ? (this.edit = true) : "";
+      setTimeout(() => {
+        this.$refs.editInput.focus()
+      }, 0)
+    },
+  },
 };
 </script>
 
